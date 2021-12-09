daea672b5840f0220c63df687957aa29645c0709
thang <thanghn2503@gamil.com>
2021-12-09 23:05:23 +0700

    ud

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000 
 const path = require('path');
+const handlebars = require('express-handlebars');
 
 
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
-    var home = path.join(__dirname,'home.html');
-    res.sendFile(home);
-})
+    res.render('home');
+});
+
+// app.get('/', (req, res) => {
+//     var home = path.join(__dirname,'home.html');
+//     res.sendFile(home);
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
