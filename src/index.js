06dcbfc2329e5d71d268eeb447e48223776902f3
thang <thanghn2503@gamil.com>
2021-12-10 08:26:44 +0700

    udt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000 
+const methodOverride = require('method-override');
 const path = require('path');
 const handlebars = require('express-handlebars');
 
@@ -19,14 +20,17 @@ app.engine('.hbs', handlebars({
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.set('trust proxy', true)
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-// app.get('/', (req, res) => {
-//     var home = path.join(__dirname,'home.html');
-//     res.sendFile(home);
-// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
