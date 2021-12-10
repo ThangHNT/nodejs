584b763be2b28cd285cbaf3c57f022b6dd2611b7
thang <thanghn2503@gamil.com>
2021-12-10 09:03:33 +0700

    sdsf

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000 
 const methodOverride = require('method-override');
+const route = require('./routes/main.js');
 const path = require('path');
 const handlebars = require('express-handlebars');
 
