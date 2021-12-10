c71cb4fe8ca4f7849934b46cd58a54594e2c994c
thang <thanghn2503@gamil.com>
2021-12-10 10:42:26 +0700

    ud

--- src/index.js
+++ src/index.js
@@ -32,8 +32,8 @@ app.use(methodOverride('_method'));
 const mongoose = require('mongoose');
 async function connect() {
     try {
-        const uri = 'mongodb://localhost:27017/practice'
-        await mongoose.createConnection(uri,{ maxPoolSize: 10000 } )
+        const uri = process.env.MONGO_PROD_URI || 'mongodb://localhost:27017/practice'
+        await mongoose.connect(uri);
         // await mongoose.connect('mongodb://localhost:27017/practice');
         console.log('Connect successfully');
     } catch (error) {
