61cb9f9f84077ea7dd47b59fc7b0573366d0bca7
thang <thanghn2503@gamil.com>
2021-12-10 08:32:34 +0700

    ud

---
+++ src/models/course.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+const mongooseDelete = require('mongoose-delete');
+
+const Schema = mongoose.Schema;
+const Course = new Schema(
+    {
+        name: { type: String, default: '', required: true },
+        description: { type: String, default: '', required: true },
+        img: { type: String, required: true },
+        videoId: { type: String, required: true },
+        slug: { type: String, slug:'name' , default: 'unknown', unique: true },
+        owner: {type: Schema.Types.ObjectId, ref: 'User'}
+    },
+    {
+        timestamps: true,
+    },
+);
+
+Course.plugin(mongooseDelete, { overrideMethods: 'all',deletedAt : true });
+mongoose.plugin(slug);
+
+module.exports = mongoose.model('Course', Course);
