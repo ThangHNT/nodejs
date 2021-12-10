2c215ddc40de1d8c5810c9b1393d6182a30279e7
thang <thanghn2503@gamil.com>
2021-12-10 08:18:25 +0700

    update

--- src/index.js
+++ src/index.js
@@ -5,9 +5,19 @@ const path = require('path');
 const handlebars = require('express-handlebars');
 
 
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.engine('.hbs', handlebars({ 
+    extname: '.hbs' ,
+    helpers: {
+        sum (a,b) { return a + b; },
+        getTimestamp (pad) {
+            pad = (n,s=2) => (`${new Array(s).fill(0)}${n}`).slice(-s);
+            const d = new Date();
+            return `${pad(d.getHours())}:${pad(d.getMinutes())} ${pad(d.getDate())}/${pad(d.getMonth()+1)}/${pad(d.getFullYear(),4)}`;
+        }
+    }
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
