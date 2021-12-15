8cea662c9e7324ae1c2f98ef88c7b5312fc336bb
thang <thanghn2503@gamil.com>
2021-12-15 11:06:55 +0700

    update

--- src/index.js
+++ src/index.js
@@ -39,20 +39,20 @@ app.use(session({
   cookie: { secure: false }
 }))
 app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.session());
 
-// passport.serializeUser(function (user, done) {
-//     done(null, user);
-// });
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
 
-// passport.deserializeUser(function (user, done) {
-//     done(null, user);
-// });
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
 
 passport.use(new FacebookStrategy({
     clientID: '610750163507271',
     clientSecret: '20360b31af2259f76484428ea92e0fd4',
-    callbackURL: "https://localhost:3000/auth/facebook/callback"
+    callbackURL: "https://courses-hnt.herokuapp.com/auth/facebook/callback"
 },
     function (accessToken, refreshToken, profile, cb) {
         return cb(null, profile);
