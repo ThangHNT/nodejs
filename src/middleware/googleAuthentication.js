1df19cfa3a1fe239b03e1634574541515b8c2da7
thang <thanghn2503@gamil.com>
2021-12-16 17:00:20 +0700

    update

--- src/middleware/googleAuthentication.js
+++ src/middleware/googleAuthentication.js
@@ -1,8 +1,10 @@
+
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 var session = require("express-session");
-app.use(passport.initialize());
-app.use(passport.session());
+
 function ggAthentication(app) {
+    app.use(passport.initialize());
+    app.use(passport.session());
     app.set('trust proxy', 1) // trust first proxy
     app.use(session({
         secret: 'keyboard cat',
@@ -10,6 +12,7 @@ function ggAthentication(app) {
         saveUninitialized: true,
         cookie: { secure: false }
     }))
+
     passport.serializeUser(function (user, done) {
         done(null, user);
     });
@@ -17,6 +20,7 @@ function ggAthentication(app) {
     passport.deserializeUser(function (user, done) {
         done(null, user);
     });
+    
     passport.use(new GoogleStrategy({
         clientID: '604856952366-efq4pjustjjib5chvsmcvofvcoko6qbt.apps.googleusercontent.com',
         clientSecret: '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',
