98c87edd9128723ffdd66122ca475b3fee4f55ce
thang <thanghn2503@gamil.com>
2021-12-16 18:09:25 +0700

    update

--- src/middleware/googleAuthentication.js
+++ src/middleware/googleAuthentication.js
@@ -40,7 +40,7 @@ function ggAthentication(app) {
     function(req, res,next) {
         const id = req.user.id;
             const name = req.user.name;
-            res.send(name + " typeof name " + typeof name);
+            res.json(req.user);
             // User.findOne({facebookId: id}, function(err, user) {
             //     if(user == null) {
             //         const user = new User({facebookId :id, email : '', username : name});
