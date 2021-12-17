6742b75fbf0d3ebb4d83d181a866c39e32ef2806
thang <thanghn2503@gamil.com>
2021-12-17 10:43:23 +0700

    update

--- src/routes/main.js
+++ src/routes/main.js
@@ -4,7 +4,7 @@ const userRouter = require('./user.js');
 const passport = require('passport');
 
 function route(app) {
-    app.use('/courses',  passport.authenticate('google', { successRedirect: '/courses',failureRedirect: '/login' }) ,courseRouter);
+    app.use('/courses',  passport.authenticate('google', { successRedirect: '/courses',failureRedirect: '/login', scope: ['profile'], session:false  }) ,courseRouter);
     app.use('/account',userRouter);
     app.use('/home',siteRouter);
     app.use('/', (req,res) => {res.render('gettingStarted')});
