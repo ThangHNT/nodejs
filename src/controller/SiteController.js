8da5e380010ad1680c3b27146292e0377099ce98
thang <thanghn2503@gamil.com>
2021-12-17 23:23:58 +0700

    update

--- src/controller/SiteController.js
+++ src/controller/SiteController.js
@@ -2,14 +2,13 @@ const Course = require('../models/course.js');
 const { multiComponents } = require('../convertToObject.js');
 class SiteController {
     home(req, res, next) {
-        // Course.find() 
-        //     .then((course) => {
-        //         res.render('home', { 
-        //             course: multiComponents(course),
-        //         })
-        //     })
-        //     .catch(next);
-        res.json(req.user);
+        Course.find() 
+            .then((course) => {
+                res.render('home', { 
+                    course: multiComponents(course),
+                })
+            })
+            .catch(next);
     }
 }
 
