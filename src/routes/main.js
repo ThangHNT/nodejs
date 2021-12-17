39a482ac40c30706aa9fa5e32bc293923da0280e
thang <thanghn2503@gamil.com>
2021-12-17 21:18:19 +0700

    update

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,7 +3,14 @@ const courseRouter = require('./course.js');
 const userRouter = require('./user.js');
 
 function route(app) {
-    app.use('/courses', courseRouter);
+    app.use('/courses',(req, res, next) => {
+        if(req.user) {
+            return next();
+        }
+        else {
+            res.status(404).render('Login-failed');
+        }
+    } , courseRouter);
     app.use('/account',userRouter);
     app.use('/home',siteRouter);
     app.use('/', (req,res) => {res.render('gettingStarted')});
