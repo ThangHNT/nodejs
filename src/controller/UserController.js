9e2029b3ce65b7bc435a59f51108e8ffcc4e465b
thang <thanghn2503@gamil.com>
2021-12-20 16:14:28 +0700

    update

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -85,23 +85,38 @@ class UserController {
 
     // submit form chỉnh sửa thông tin cá nhân
     uploadData(req, res, next) {
-        var img = fs.readFileSync(req.file.path);   // lấy đg dẫn file ảnh
-        var encode_img = img.toString('base64');    // chuyển về dạng base64
-        const buffer = Buffer.from(encode_img,'base64');    // cho ảnh vào bufer
-        var final_img = {                           // tạo obj Img để lưu vào db
-            name: req.file.originalname,
-            img : {
-                contentType:req.file.mimetype,
-                data:img,
-                image: buffer
+        // dung voi multer
+        // var img = fs.readFileSync(req.file.path);   // lấy đg dẫn file ảnh
+        // var encode_img = img.toString('base64');    // chuyển về dạng base64
+        // const buffer = Buffer.from(encode_img,'base64');    // cho ảnh vào bufer
+        // var final_img = {                           // tạo obj Img để lưu vào db
+        //     name: req.file.originalname,
+        //     img : {
+        //         contentType:req.file.mimetype,
+        //         data:img,
+        //         image: buffer
+        //     }
+        // };
+        // const image = new Img(final_img);
+        // image.save() 
+        //     .then(() => {
+        //         res.redirect(`/account/uploaded/${image._id}`)  
+        //     })
+        //     .catch(next);
+
+        // dung voi formidable
+        var formidable = require('formidable');
+        var form = new formidable.IncomingForm();
+        form.uploadDir = "./src/puclic/uplpoads";
+        form.keepExtension = true;
+        form.maxFieldSize = 1024*1024;
+        form.multiples = true;
+        form.parse(req,(err,fields, files)=> {
+            if(err) {
+                res.send('co loi');
             }
-        };
-        const image = new Img(final_img);
-        image.save() 
-            .then(() => {
-                res.redirect(`/account/uploaded/${image._id}`)  
-            })
-            .catch(next);
+            var arrayFiles = files[""];
+        })
     }
 
     // sau khi cập nhật dữ liệu 
