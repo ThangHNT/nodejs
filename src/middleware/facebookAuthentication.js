53c6568a1f9512112dba7248f18fe7115b838b0c
thang <thanghn2503@gamil.com>
2021-12-22 18:10:02 +0700

    update

--- src/middleware/facebookAuthentication.js
+++ src/middleware/facebookAuthentication.js
@@ -43,8 +43,8 @@ function authenticate(app) {
             const id = req.user.id;
             const name = req.user.displayName;
             const avatar = req.user.photos[0].value;
+            req.setHeader('user',id);
             User.findOne({facebookId: id}, function(err, user) {
-                res.setHeader('user',id);
                 if(user == null) {
                     const user = new User({facebookId :id, email : '', username : name,authType: 'facebook', avatar: avatar});
                     user.save()
