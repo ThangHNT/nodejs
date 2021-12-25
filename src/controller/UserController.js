962b5c2ad37c5500c9a364ab57ed07c77b7d6929
thang <thanghn2503@gamil.com>
2021-12-25 10:54:00 +0700

    update

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -63,10 +63,11 @@ class UserController {
         var id = req.user.id;
         User.findOne({id: id,authType: provider}, function(err, user) {
             Img.findOne({owner: user._id}, function(err, img) {
+                var avt = (img.img.data) ? img.img.data : null;
                 return res.render('myAccount', {
                     user : component(user),
                     avatar : img.src,
-                    avatar_base64: img.img.data,
+                    avatar_base64: avt
                 })
             })
         })
@@ -78,10 +79,11 @@ class UserController {
         var id = req.user.id;
         User.findOne({id: id,authType: provider}, function(err, user) {
             Img.findOne({owner: user._id}, function(err, img) {
+                var avt = (img.img.data) ? img.img.data : null;
                 return res.render('updateProfile', {
                     user : component(user),
                     avatar : img.src,
-                    avatar_base64: img.img.data,
+                    avatar_base64: avt,
                 })
             })
         })
