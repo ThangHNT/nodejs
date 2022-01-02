9d3bd1343c95022264f1da5b2d2e598363a218b8
thang <thanghn2503@gamil.com>
2022-01-02 10:24:54 +0700

    ud

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.engine('.hbs', handlebars({
             var month = date.getMonth() + 1;
             var year  = date.getFullYear();
             if(month < 10) month = '0'+ month;
-            if(day < 10) day = '0' + day.toString;
+            if(day < 10) day = '0' + day;
             return `${day}/${month}/${year}`;
         },
         dateofbirth(dob){
